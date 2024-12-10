682a367c6a0ce485bcb776e79d92814a78d2466c
itsjustinetime <justinetime2023@gmail.com>
2024-12-10 01:53:21 +0000

    Added fsclose div. To uhh, let us close the fs modal

--- templates/venue.php
+++ templates/venue.php
@@ -9,23 +9,26 @@
 
 </div>
 	<div class="venuemodal" id="fs{venuetag}">
-		<div class="closebutton"><i class="fa-regular fa-circle-xmark fa-2xl"></i></div>
-	<div class="venueimage"><img class="fsvenueimage" imgsrc="{venueimage}"></img></div>
-		<div class="venuecolltext">
-		<div class="fsvenuetitle">{venuename}</div>
-		<div class="fstit" style="display:{venuerecshow}"><i class="fa-regular fa-star"></i>LFF recommended</div>
-			<div class="fsinfotext white">{venuedescription}</div> 
-				{OFFER}
-			<div class="venuesociallinks">
-			<a class="slink" href="{venuefacebook}" name="vfb" style="display:none;" id="vfacebook{venuetag}"><i class="fa-brands fa-facebook"></i></a>
-			<a class="slink" href="{venueinstagram}" name="vig" style="display:none;" id="vinstagram{venuetag}"><i class="fa-brands fa-instagram"></i></a>
-			<a class="slink" href="{venuewebsite}" name="vweb" style="display:none;" id="vwebsite{venuetag}"><i class="fa-solid fa-link"></i></a>
+		<div id="fsclose" onClick="destroyFullscreen()">
+			<div class="closebutton"><i class="fa-regular fa-circle-xmark fa-2xl"></i></div>
+			<div class="venueimage"><img class="fsvenueimage" imgsrc="{venueimage}"></img></div>
+			<div class="venuecolltext">
+				<div class="fsvenuetitle">{venuename}</div>
+				<div class="fstit" style="display:{venuerecshow}"><i class="fa-regular fa-star"></i>LFF recommended</div>
+					<div class="fsinfotext white">{venuedescription}</div> 
+						{OFFER}
+					<div class="venuesociallinks">
+					<a class="slink" href="{venuefacebook}" name="vfb" style="display:none;" id="vfacebook{venuetag}"><i class="fa-brands fa-facebook"></i></a>
+					<a class="slink" href="{venueinstagram}" name="vig" style="display:none;" id="vinstagram{venuetag}"><i class="fa-brands fa-instagram"></i></a>
+					<a class="slink" href="{venuewebsite}" name="vweb" style="display:none;" id="vwebsite{venuetag}"><i class="fa-solid fa-link"></i></a>
+					</div>
+
+				
 			</div>
-			<div class="mapbutton">
-					<a id="maplink" href='https://maps.google.com/?q={venuecoords}'>
-						<div class="fsaddress white">{venueaddress}<i class="fsdot fa-solid fa-location-dot white"></i></div>
-					</a>
-			</div>
-			
 		</div>
+						<div class="mapbutton" id="map{venuetag}">
+						<a class="mapbutton_a" onClick="mapTranslate(this.id)" id="maplink{venuetag}" href='https://maps.google.com/?q={venuecoords}'>
+							<div class="fsaddress white">{venueaddress}<i class="fsdot fa-solid fa-location-dot white"></i></div>
+						</a>
+				</div>
 	</div>
