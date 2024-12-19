e1b3ce0912701e3fae760446c2a9b5f03ab84a53
itsjustinetime <justinetime2023@gmail.com>
2024-12-19 00:54:16 +0000

    tidied up a bit

--- functions/functions.php
+++ functions/functions.php
@@ -4,20 +4,18 @@ include_once '../configuration.php';
 function getPasscodes() {
 	$timeDate = date("Y-m-d H:i:s");
 	global $PATH_CONTENT;
-	error_log("getPasscodes");
 	// read non-expired LFF passcodes into an array
-$lffCodes[]='';
-if (!empty(glob($PATH_CONTENT . '/lff-events/passcodes/*.json'))) {
-	//error_log("glob not empty");
-        foreach (glob($PATH_CONTENT . '/lff-events/passcodes/*.json') as $key => $file) {
-            $data = json_decode(file_get_contents($file));
-                  $passcodevalue=$data->passcodevalue;
-                  $passcodeexpires=str_replace("T"," ", $data->passcodeexpires);
-				 // error_log("passcode: ".$passcodevalue . " expires:".$passcodeexpires);
-		if ($passcodeexpires > $timeDate) { $lffCodes[]=$passcodevalue; }
+	$lffCodes[]='';
+	if (!empty(glob($PATH_CONTENT . '/lff-events/passcodes/*.json'))) {
+		//error_log("glob not empty");
+			foreach (glob($PATH_CONTENT . '/lff-events/passcodes/*.json') as $key => $file) {
+				$data = json_decode(file_get_contents($file));
+					  $passcodevalue=$data->passcodevalue;
+					  $passcodeexpires=str_replace("T"," ", $data->passcodeexpires);
+			if ($passcodeexpires > $timeDate) { $lffCodes[]=$passcodevalue; }
+		}
 	}
-}
-return $lffCodes;
+	return $lffCodes;
 	
 }
 
